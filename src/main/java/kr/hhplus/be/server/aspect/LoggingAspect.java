74e625bb56fe14ab60ff43fa0de855f8e6234f93
katiekim17 <pyeongsuk.kim88@gmail.com>
2025-04-21 00:38:10 +0900

    feat: AOP 로직 구현

---
+++ src/main/java/kr/hhplus/be/server/aspect/LoggingAspect.java
@@ -0,0 +1,23 @@
+package kr.hhplus.be.server.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Before("execution(* kr.hhplus.be.server..*Service.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println(">> 서비스 시작: " + joinPoint.getSignature());
+    }
+
+    @AfterReturning("execution(* kr.hhplus.be.server..*Service.*(..))")
+    public void logAfter() {
+        System.out.println("<< 서비스 끝");
+    }
+
+}
